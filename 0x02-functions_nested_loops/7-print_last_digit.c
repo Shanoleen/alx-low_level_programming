eecc7a5d661e625bd095c1736c5e37f7d8fe9759
Shanoleen <shanoleen@live.com>
2022-10-19 11:45:48 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,16 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int pld;
 
-	if (ld < 0)
-		ld *= -1;
+	pld = (n % 10);
 
-	_putchar(ld + '0');
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
 
-	return (0);
+	_putchar(pld + '0');
+
+	return (pld);
 }
