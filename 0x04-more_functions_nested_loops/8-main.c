ba564d85f9756b8da3b36eca82395414d5242da4
Shanoleen <shanoleen@live.com>
2022-10-21 16:09:10 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * main - check the code*
  * Return: Always 0.
  */
 
@@ -11,5 +10,5 @@ int main(void)
 	print_square(2);
 	print_square(10);
 	print_square(0);
-       	return (0);
+	return (0);
 }
